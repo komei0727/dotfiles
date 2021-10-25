eb433c7bd0de7135ef2b6f27e42d24304206a081
komei0727 <komei0727@gmail.com>
2021-10-25 17:20:59 +0900

    Modify

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-dotfiles=(.zshrc .vimrc)
-
-# .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
-# ホームディレクトリ直下に作成する
-for file in "${dotfiles[@]}"; do
-        ln -svf $file ~/
-done
+ln -sv ~/dotfiles/.zshrc ~/
+ln -sv ~/dotfiles/.vimrc ~/
